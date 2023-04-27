c6c0147823231aae4778b36e36d6ac2b998de3a2
LEEJAECHUN <leejc831@naver.com>
2023-04-27 14:08:27 +0900

    금액 조회

--- src/components/Card_01.vue
+++ src/components/Card_01.vue
@@ -1,28 +1,42 @@
 <template>
 <div class="card shadow-sm">
+  <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}"></span>
     <img :src="item.imgPath"/>
     
     <div class="card-body">
         <p class="card-text">{{ item.name }}</p>
         <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-body-secondary">9 mins</small>
+        <button class="btn btn-primary">구입하기</button>
+        <small class="price text-muted">{{lib.getNumberFormatted(item.price)}}원</small>
+        <span class="discount badge bg-danger">{{item.discount_per}}%</span >
+        <small class="real text-danger">{{lib.getNumberFormatted(item.price - (item.price * item.discount_per / 100 ))}}원</small>
         </div>
     </div>
     </div>
 </template>
-
 <script>
+import lib from "@/scripts/lib"
 export default {
   name: 'Card_01', // eslint-disable-next-line
   props: {
     item: Object
+  },
+  setup(){
+    return {lib}
   }
 }
 </script>
 
 <style scoped>
+.card img{
+  display: block;
+  width:100%;
+  height: 250px;
+  background-size:cover;
+  background-position:center;
+}
+
+.card .card-body .price {
+  text-decoration: line-through;
+}
 </style>
--- src/pages/Home_01.vue
+++ src/pages/Home_01.vue
@@ -1,17 +1,5 @@
 <template>
 <div class="home">
-  <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">VUE3 연습</h1>
-        <p class="lead text-body-secondary">vue3 - 스트링부트로 만든 페이지</p>
-        <p>
-          <a href="#" class="btn btn-primary my-2">메뉴1</a>
-          <a href="#" class="btn btn-secondary my-2">메뉴2</a>
-        </p>
-      </div>
-    </div>
-  </section>
 
   <div class="album py-5 bg-body-tertiary">
     <div class="container">
