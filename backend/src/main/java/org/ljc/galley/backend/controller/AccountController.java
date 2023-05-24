14faaca9bf4c510cbb987b2fd9ed9f9f33be6685
LEEJAECHUN <leejc831@naver.com>
2023-05-24 10:37:22 +0900

    로그인 JWT 토큰 처리

--- backend/src/main/java/org/ljc/galley/backend/controller/AccountController.java
+++ backend/src/main/java/org/ljc/galley/backend/controller/AccountController.java
@@ -1,10 +1,16 @@
 package org.ljc.galley.backend.controller;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.ljc.galley.backend.entity.Member;
 import org.ljc.galley.backend.repository.MemberRepository;
+import org.ljc.galley.backend.service.JwtService;
+import org.ljc.galley.backend.service.JwtServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +23,21 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params){
+    public ResponseEntity login(@RequestBody Map<String, String> params, HttpServletResponse response){
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
         if(member != null){
-            return member.getId();
+            JwtService jwtService = new JwtServiceImpl();
+            int id =  member.getId();
+            System.out.println("11111111111111 = " + id);
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie(token, token);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            response.addCookie(cookie);
+            return new ResponseEntity<>(id, HttpStatus.OK);
         }
 
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
--- backend/src/main/java/org/ljc/galley/backend/service/JwtServiceImpl.java
+++ backend/src/main/java/org/ljc/galley/backend/service/JwtServiceImpl.java
@@ -1,21 +1,37 @@
 package org.ljc.galley.backend.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+import java.security.Signature;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 public class JwtServiceImpl implements JwtService{
 
-    private String secretKey = "adccvdf@@@dfaf4566548787fwee87f878e7";
+    private String secretKey = "adccvdf@@@dfaf4566548787fwee87f878e7ADSFAF@#66548787fwee87f878e7SA66548787fwee87f878e7";
     @Override
     public String getToken(String key, Object Value){
         
         Date expTime = new Date();
         expTime.setTime(expTime.getTime() + 1000 * 60 * 5);
         byte[] secretByteKey = DatatypeConverter.parseBase64Binary(secretKey);
+        Key signKey = new SecretKeySpec(secretByteKey,SignatureAlgorithm.HS256.getJcaName());
         
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("typ","JWT");
+        headerMap.put("alg","HS256");
 
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        map.put(key, Value);
+
+        JwtBuilder builder = Jwts.builder().setHeader(headerMap).setClaims(map).setExpiration(expTime).signWith(signKey, SignatureAlgorithm.HS256);
+
+        return builder.compact();
     }
 }
